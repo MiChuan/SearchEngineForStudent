0220aee11e0861e93868ae99b306c6fe1be00162
Hu_Han <1102066475@qq.com>
2020-04-11 11:47:50 +0800

    package parse have done, need to do 'doucumentbuilder,index,indexbuilder' and check package index

--- src/hust/cs/javacourse/search/parse/impl/LengthTermTupleFilter.java
+++ src/hust/cs/javacourse/search/parse/impl/LengthTermTupleFilter.java
@@ -1,4 +1,34 @@
 package hust.cs.javacourse.search.parse.impl;
 
-public class LengthTermTupleFilter {
+import hust.cs.javacourse.search.index.AbstractTermTuple;
+import hust.cs.javacourse.search.parse.AbstractTermTupleFilter;
+import hust.cs.javacourse.search.parse.AbstractTermTupleStream;
+import hust.cs.javacourse.search.util.Config;
+
+public class LengthTermTupleFilter extends AbstractTermTupleFilter {
+    /**
+     * 构造函数
+     * @param input : 输入文件流
+     * */
+    public LengthTermTupleFilter(AbstractTermTupleStream input) {
+        super(input);
+    }
+
+    /**
+     * 获得下一个三元组
+     * 过滤长度小于3或大于20的单词
+     * @return: 下一个三元组；如果到了流的末尾，返回null
+     */
+    @Override
+    public AbstractTermTuple next() {
+        AbstractTermTuple lengthFilter = input.next();
+        if(lengthFilter == null) return null;
+        //长度小于3或大于20的被过滤
+        while (lengthFilter.term.getContent().length()< Config.TERM_FILTER_MINLENGTH ||
+                lengthFilter.term.getContent().length()>Config.TERM_FILTER_MAXLENGTH){
+            lengthFilter = input.next();
+            if(lengthFilter==null) return null;
+        }
+        return lengthFilter;
+    }
 }
--- src/hust/cs/javacourse/search/parse/impl/PatternTermTupleFilter.java
+++ src/hust/cs/javacourse/search/parse/impl/PatternTermTupleFilter.java
@@ -1,4 +1,35 @@
 package hust.cs.javacourse.search.parse.impl;
 
-public class PatternTermTupleFilter {
+import hust.cs.javacourse.search.index.AbstractTermTuple;
+import hust.cs.javacourse.search.parse.AbstractTermTupleFilter;
+import hust.cs.javacourse.search.parse.AbstractTermTupleStream;
+import hust.cs.javacourse.search.util.Config;
+
+public class PatternTermTupleFilter extends AbstractTermTupleFilter {
+    private String pattern;
+
+    /**
+     * 构造函数
+     * @param input : 输入流
+     * */
+    public PatternTermTupleFilter(AbstractTermTupleStream input) {
+        super(input);
+        pattern = Config.TERM_FILTER_PATTERN;
+    }
+
+    /**
+     * 获得下一个三元组
+     * 基于正则表达式，过滤非英文字符
+     * @return: 下一个三元组；如果到了流的末尾，返回null
+     */
+    @Override
+    public AbstractTermTuple next() {
+        AbstractTermTuple patternFilter = input.next();
+        if(patternFilter==null) return null;
+        while (!patternFilter.term.getContent().matches(pattern)){//不匹配，含有非英文字符
+            patternFilter = input.next();
+            if(patternFilter==null) return null;
+        }
+        return patternFilter;
+    }
 }
